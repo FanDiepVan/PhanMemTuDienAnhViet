db765965610fff1a4c2a4ab801f3e874bb42c337
DinhSa <kaioshin95@gmail.com>
2016-11-06 13:47:58 +0700

    up

---
+++ ThietKeGiaoDien/TrangChu.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace ThietKeGiaoDien
+{
+    partial class TrangChu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(819, 5);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(80, 25);
+            this.button5.TabIndex = 14;
+            this.button5.Text = "Thông Tin";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(744, 5);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(76, 25);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Trợ Giúp";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(666, 5);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(78, 25);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "Gửi Phản Hồi";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // TrangChu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 503);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Name = "TrangChu";
+            this.Text = "TrangChu";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+    }
+}
