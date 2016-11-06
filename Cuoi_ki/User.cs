b611f38808f2e88d72cc14e7a5e5e04146a7514d
daihuynh2010 <daihuynh2010@gmail.com>
2016-11-06 13:30:04 +0700

    Add files via upload

---
+++ Cuoi_ki/User.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cuoi_ki
+{
+    public partial class User : Form
+    {
+        public User()
+        {
+            InitializeComponent();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            QuanLiTaiKhoan ql = new QuanLiTaiKhoan();
+            ql.Show();
+        }
+    }
+}
