0c0414c426c73440a29d07b7c357471fce7a3a68
tuananh9496 <nguyentuananh9496@gmail.com>
2016-11-07 14:10:29 +0700

    Thêm màn hình "Đọc phản hồi" và "Xem trang quản lý"
    Khác framework nên nó sửa nhiều thứ. Không biết có ảnh hưởng lắm không.

---
+++ ThietKeGiaoDien/frm_docphanhoi.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace ThietKeGiaoDien
+{
+    partial class frm_docphanhoi
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_huy = new System.Windows.Forms.Button();
+            this.btn_TraLoi = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_huy
+            // 
+            this.btn_huy.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_huy.Location = new System.Drawing.Point(473, 463);
+            this.btn_huy.Name = "btn_huy";
+            this.btn_huy.Size = new System.Drawing.Size(94, 31);
+            this.btn_huy.TabIndex = 49;
+            this.btn_huy.Text = "Quay lại";
+            this.btn_huy.UseVisualStyleBackColor = true;
+            // 
+            // btn_TraLoi
+            // 
+            this.btn_TraLoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_TraLoi.Location = new System.Drawing.Point(325, 463);
+            this.btn_TraLoi.Name = "btn_TraLoi";
+            this.btn_TraLoi.Size = new System.Drawing.Size(102, 31);
+            this.btn_TraLoi.TabIndex = 48;
+            this.btn_TraLoi.Text = "Trả lời";
+            this.btn_TraLoi.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            this.label7.BackColor = System.Drawing.Color.Gray;
+            this.label7.Location = new System.Drawing.Point(9, 437);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(886, 10);
+            this.label7.TabIndex = 47;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(124, 245);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(176, 23);
+            this.label6.TabIndex = 46;
+            this.label6.Text = "Nội Dung Phản Hồi";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(124, 187);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(72, 23);
+            this.label5.TabIndex = 44;
+            this.label5.Text = "Email:";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(124, 152);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(81, 23);
+            this.label4.TabIndex = 41;
+            this.label4.Text = "Người gửi:";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(9, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(886, 10);
+            this.label3.TabIndex = 40;
+            // 
+            // label2
+            // 
+            this.label2.Enabled = false;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(319, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(232, 33);
+            this.label2.TabIndex = 39;
+            this.label2.Text = "ĐỌC PHẢN HỒI";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(14, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(175, 56);
+            this.label1.TabIndex = 38;
+            this.label1.Text = "D-P-A ";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(226, 152);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(162, 23);
+            this.label8.TabIndex = 50;
+            this.label8.Text = "Đinh Vĩnh Phúc";
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(226, 187);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(162, 23);
+            this.label9.TabIndex = 50;
+            this.label9.Text = "dinhvinhphuc@gmail.com";
+            // 
+            // label10
+            // 
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(124, 283);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(660, 86);
+            this.label10.TabIndex = 51;
+            this.label10.Text = "Phần mềm cần bổ sung thêm nhiều tính năng như nghe nhạc kết hợp với lời bài hát.." +
+    ".";
+            // 
+            // frm_docphanhoi
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 503);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_huy);
+            this.Controls.Add(this.btn_TraLoi);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_docphanhoi";
+            this.Text = "frm_docphanhoi";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_huy;
+        private System.Windows.Forms.Button btn_TraLoi;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+
+    }
+}
---
+++ ThietKeGiaoDien/frm_trangquanly.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace ThietKeGiaoDien
+{
+    partial class frm_trangquanly
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btn_DocPhanHoi = new System.Windows.Forms.Button();
+            this.btn_QuanLyTaiLieu = new System.Windows.Forms.Button();
+            this.btn_QuanLyTKThanhVien = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btn_DangXuat = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.BackColor = System.Drawing.Color.Gray;
+            this.label4.Location = new System.Drawing.Point(7, 444);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(886, 10);
+            this.label4.TabIndex = 24;
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.Gray;
+            this.label3.Location = new System.Drawing.Point(12, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(886, 10);
+            this.label3.TabIndex = 23;
+            // 
+            // btn_DocPhanHoi
+            // 
+            this.btn_DocPhanHoi.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_DocPhanHoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DocPhanHoi.Location = new System.Drawing.Point(361, 323);
+            this.btn_DocPhanHoi.Name = "btn_DocPhanHoi";
+            this.btn_DocPhanHoi.Size = new System.Drawing.Size(128, 46);
+            this.btn_DocPhanHoi.TabIndex = 22;
+            this.btn_DocPhanHoi.Text = "Đọc phản hồi";
+            this.btn_DocPhanHoi.UseVisualStyleBackColor = false;
+            // 
+            // btn_QuanLyTaiLieu
+            // 
+            this.btn_QuanLyTaiLieu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_QuanLyTaiLieu.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_QuanLyTaiLieu.Location = new System.Drawing.Point(360, 261);
+            this.btn_QuanLyTaiLieu.Name = "btn_QuanLyTaiLieu";
+            this.btn_QuanLyTaiLieu.Size = new System.Drawing.Size(128, 46);
+            this.btn_QuanLyTaiLieu.TabIndex = 21;
+            this.btn_QuanLyTaiLieu.Text = "Quản lý tài liệu";
+            this.btn_QuanLyTaiLieu.UseVisualStyleBackColor = false;
+            // 
+            // btn_QuanLyTKThanhVien
+            // 
+            this.btn_QuanLyTKThanhVien.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_QuanLyTKThanhVien.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_QuanLyTKThanhVien.Location = new System.Drawing.Point(360, 203);
+            this.btn_QuanLyTKThanhVien.Name = "btn_QuanLyTKThanhVien";
+            this.btn_QuanLyTKThanhVien.Size = new System.Drawing.Size(128, 45);
+            this.btn_QuanLyTKThanhVien.TabIndex = 20;
+            this.btn_QuanLyTKThanhVien.Text = "Quản lý tài khoản thành viên";
+            this.btn_QuanLyTKThanhVien.UseVisualStyleBackColor = false;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(355, 102);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 47);
+            this.label2.TabIndex = 19;
+            this.label2.Text = "ENG-VIE";
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.label1.Location = new System.Drawing.Point(232, 30);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(401, 56);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "D-P-A DICTIONARY";
+            // 
+            // btn_DangXuat
+            // 
+            this.btn_DangXuat.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.btn_DangXuat.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_DangXuat.Location = new System.Drawing.Point(361, 382);
+            this.btn_DangXuat.Name = "btn_DangXuat";
+            this.btn_DangXuat.Size = new System.Drawing.Size(128, 46);
+            this.btn_DangXuat.TabIndex = 25;
+            this.btn_DangXuat.Text = "Đăng xuất";
+            this.btn_DangXuat.UseVisualStyleBackColor = false;
+            // 
+            // frm_trangquanly
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 503);
+            this.Controls.Add(this.btn_DangXuat);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_DocPhanHoi);
+            this.Controls.Add(this.btn_QuanLyTaiLieu);
+            this.Controls.Add(this.btn_QuanLyTKThanhVien);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frm_trangquanly";
+            this.Text = "frm_trangquanly";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btn_DocPhanHoi;
+        private System.Windows.Forms.Button btn_QuanLyTaiLieu;
+        private System.Windows.Forms.Button btn_QuanLyTKThanhVien;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btn_DangXuat;
+    }
+}
